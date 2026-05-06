95cb5b8bffc56506090383fe740e3afd55d4f75f
rayos-vt <143996910+rayos-vt@users.noreply.github.com>
2026-05-05 22:27:28 -0500

    service worker functionality

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(caches => {
+            return caches.addAll([
+                "./", "./styles/styles.css", 
+                "./images/logo192.webp"
+            ]);
+        })
+    );
+});
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
