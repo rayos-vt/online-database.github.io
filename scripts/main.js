ff2d5a055d9e89087ad5a2783c6cc777ea8baa4b
rayos-vt <143996910+rayos-vt@users.noreply.github.com>
2026-05-05 22:29:08 -0500

    service worker functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log('SW Registered');
+        console.log(registration);
+    }).catch(error => {
+        console.log('SW Registration Failed');
+        console.log(error);
+    });
+}
+
 const SELECT = 'SELECT * FROM imagedata';
 const slideshow = document.getElementById('slideshow');
 const startScreen = document.getElementById('startScreen');
